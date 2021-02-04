8b43650a354d4a7b6aba62d50bda8e64779f13bd
Danfeng Wang <dannywang@utexas.edu>
2021-02-04 15:34:05 -0500

    add SHOW_ALL scale and CSS - make margin 0

--- state0.js
+++ state0.js
@@ -5,15 +5,8 @@ demo.state0.prototype = {
     create: function () {
         game.stage.backgroundColor = "#80ff80";
         console.log("state0");
-        // game.input.keyboard
-        //     .addKey(Phaser.Keyboard.ONE)
-        //     .onDown.add(changeState, null, null, 1);
+        game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 
-        // game.input.keyboard
-        //     .addKey(Phaser.Keyboard.TWO)
-        //     .onDown.add(changeState, null, null, 2);
-
-        // addKeyCallback(Phaser.Keyboard.ZERO, changeState, 0);
         addChangeStateEventListeners();
     },
     update: function () {},
