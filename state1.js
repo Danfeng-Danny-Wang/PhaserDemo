0bd945c50ebb658229322745f935e259f110dd93
Danfeng Wang <dannywang@utexas.edu>
2021-02-04 22:04:07 -0500

    add tile maps: rocks and grass

--- state1.js
+++ state1.js
@@ -1,10 +1,26 @@
 demo.state1 = function () {};
 demo.state1.prototype = {
-    preload: function () {},
+    preload: function () {
+        game.load.tilemap(
+            "field",
+            "assets/tilemaps/field.json",
+            null,
+            Phaser.Tilemap.TILED_JSON
+        );
+        game.load.image("grassTiles", "assets/tilemaps/grassTiles.png");
+        game.load.image("rockTiles", "assets/tilemaps/rockTiles.png");
+    },
     create: function () {
         game.stage.backgroundColor = "#DDDDDD";
 
         addChangeStateEventListeners();
+
+        var map = game.add.tilemap("field");
+        map.addTilesetImage("grassTiles");
+        map.addTilesetImage("rockTiles");
+
+        var grass = map.createLayer("grass");
+        var rocks = map.createLayer("rocks");
     },
     update: function () {},
 };
